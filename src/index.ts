e7abd9fe8c85af3125047fc1d78ed109d81e464f
jcake <jakieschneider13@gmail.com>
2022-06-09 23:53:12 +1000

    Build Script Working, basic app in place.
    See BUILDING.md if any issues arise during the build process.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import * as ng from 'nodegui';
 import chalk from 'chalk';
+import _ from 'lodash';
+
+import parse from './config.js';
+import ui from './ui.js';
+
+export interface Config {
+    default: string
+}
+
+export const config: Config = {} as any;
 
 export default async function main(argv: string[]): Promise<number> {
+    _.merge(config, parse(argv.slice(2)));
+
     const window = new ng.QMainWindow();
+
+    ui(window);
+
+    window.show();
 
     return 0;
 }
