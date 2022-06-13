1939fc323a03d0a51884ba5e291bd965ce9c9629
jcake <jakieschneider13@gmail.com>
2022-06-13 22:50:18 +1000

    Works under @nodegui/nodegui@0.53.0

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,20 @@ import * as ng from '@nodegui/nodegui';
 import _ from 'lodash';
 
 import parse from './config.js';
-// import { initLocales, Locale, translate } from './locale.js';
+import { initLocales, Locale, translate } from './locale.js';
 import { LogLevel } from './log.js';
 import { editor, menubar } from './ui/index.js';
 
 export interface Config {
-    // locale: StateManager<Record<string, Locale>>,
+    locale: StateManager<Record<string, Locale>>,
     activeLocale: string,
     logLevel: LogLevel,
 }
 
 export const config: StateManager<Config> = new StateManager({
     // activeLocale: 'de_DE',
-    // activeLocale: 'en_GB',
-    // locale: await initLocales()
+    activeLocale: 'en_GB',
+    locale: await initLocales()
 } as Config);
 
 export default async function main(argv: string[]): Promise<number> {
--- src/ui/editor.ts
+++ src/ui/editor.ts
@@ -1,15 +1,18 @@
 import * as ng from '@nodegui/nodegui';
+
 import { translate } from '../locale.js';
 
 export default function editor(window: ng.QMainWindow): ng.QWidget {
     const widget = new ng.QWidget();
+
     const layout = new ng.QBoxLayout(ng.Direction.TopToBottom);
+    
+    const checkbox = new ng.QCheckBox();
+    checkbox.setText(`hi`);
+
+    layout.addWidget(checkbox);
+
     widget.setLayout(layout);
-
-    const openLabel = new ng.QLabel();
-    openLabel.setText(`Open Notebook`);
-    layout.addWidget(openLabel);
-    openLabel.setText(translate`Open a Notebook to begin`);
-
+    
     return widget;
 }
